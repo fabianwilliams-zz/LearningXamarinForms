f37499462e09562ac7105d93119f7c4c30c595d2
Fabian Williams <fabian@adotob.com>
2014-11-16 21:23:20 -0500

    Adding stuff working on

--- FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
+++ FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -11,27 +11,10 @@ namespace Greetings
         public GreetingsPage()
         {
             FormattedString formattedString = new FormattedString();
-
-			formattedString.Spans.Add(new Span
-				{
-					Text = "Fabian is gonna "
-				});
-			formattedString.Spans.Add(new Span
-				{
-					Text = "ROCK!",
-					Font = Font.SystemFontOfSize(NamedSize.Large, FontAttributes.Bold)
-				});
-			formattedString.Spans.Add(new Span
-				{
-					Text = " Xamarin.Forms!"
-				});
-
             //Label label = new Label // taking this away in second step to make it this.Content
             this.Content = new Label
             {
-                
-				//Text = "Hi from Fabian Williams Xamarin Forms",-- going to use Fomatted Text now
-				FormattedText = formattedString,
+                Text = "Hi from Fabian Williams Xamarin Forms",
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 BackgroundColor = Color.Purple,
