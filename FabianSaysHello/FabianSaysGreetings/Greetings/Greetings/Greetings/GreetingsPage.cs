74fedbbee20a5c7888d586f0c25e3fcd9558b48e
Fabian Williams <fabian@adotob.com>
2014-11-16 21:22:24 -0500

    Added Formatted String
    This didn’t work well on Visual Studio side with ReSharper because SAP
    doesn’t have references so didn’t allow me to add using statement.

--- FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
+++ FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -11,10 +11,27 @@ namespace Greetings
         public GreetingsPage()
         {
             FormattedString formattedString = new FormattedString();
+
+			formattedString.Spans.Add(new Span
+				{
+					Text = "Fabian is gonna "
+				});
+			formattedString.Spans.Add(new Span
+				{
+					Text = "ROCK!",
+					Font = Font.SystemFontOfSize(NamedSize.Large, FontAttributes.Bold)
+				});
+			formattedString.Spans.Add(new Span
+				{
+					Text = " Xamarin.Forms!"
+				});
+
             //Label label = new Label // taking this away in second step to make it this.Content
             this.Content = new Label
             {
-                Text = "Hi from Fabian Williams Xamarin Forms",
+                
+				//Text = "Hi from Fabian Williams Xamarin Forms",-- going to use Fomatted Text now
+				FormattedText = formattedString,
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.StartAndExpand,
                 BackgroundColor = Color.Purple,
