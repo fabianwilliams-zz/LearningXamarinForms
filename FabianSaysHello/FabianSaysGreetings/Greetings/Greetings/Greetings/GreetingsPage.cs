f7d03881bce428ca2d14d25178337720f6487d61
Fabian Williams <fabian@adotob.com>
2014-11-16 21:09:39 -0500

    Added a few Color Properties

--- FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
+++ FabianSaysHello/FabianSaysGreetings/Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Xamarin.Forms; 
+using Xamarin.Forms;
+
 
 namespace Greetings
 {
@@ -9,14 +10,19 @@ namespace Greetings
     {
         public GreetingsPage()
         {
+            FormattedString formattedString = new FormattedString();
             //Label label = new Label // taking this away in second step to make it this.Content
             this.Content = new Label
             {
-                Text = "Hello from Fabian Williams Xamarin Forms"
+                Text = "Hi from Fabian Williams Xamarin Forms",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                BackgroundColor = Color.Purple,
+                TextColor = Color.Yellow
             };
 
-            this.Padding = new Thickness (0, 20, 0, 0); //this way makes it happen on all devices
-            //this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0); //iOS only device
+            //this.Padding = new Thickness (0, 20, 0, 0); //this way makes it happen on all devices
+            this.Padding = new Thickness(0, Device.OnPlatform(20, 0, 0), 0, 0); //iOS only device
 
         }
     }
